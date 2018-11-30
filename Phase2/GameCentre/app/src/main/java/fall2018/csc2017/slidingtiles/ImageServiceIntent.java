53e366bc1a2c4401dbce6072aa0c91ed2659dbec
likevi23 <kevinbobby.li@mail.utoronto.ca>
2018-11-30 16:29:50 -0500

    Deleted unused imports in ImageServiceIntent

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ImageServiceIntent.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ImageServiceIntent.java
@@ -11,9 +11,7 @@ import android.support.annotation.Nullable;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 
 /**
  * modified intent service to handle splitting an image
